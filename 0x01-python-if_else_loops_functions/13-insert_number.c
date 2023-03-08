b5e9f5a20374d438d6d70d8d547d72d2c28ec838
Hermelab1 <hermelabirhanu06@gmail.com>
2023-03-07 18:36:46 -0800

    Fix on task 12 and 13

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,52 +1,67 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * createNode - create a new node
- *
- * @prmNbr: number
- *
- * Return: new element
- */
-listint_t *createNode(const int prmNbr)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = prmNbr;
-
-	return (new);
-}
-
-/**
- * add_nodeint - adds a new node at the beginning of a list_t list
- *
- * @head: pointer to last element
- * @n: number
- *
- * Return: new last element
+ * insert_node - function to insert new node in sorted linked list
+ * @head: input pointer to head pointer of singly linked list
+ * @number: input number to sort and insert into list as new node
+ * Return: pointer to newly added node or NULL if failed
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new;
+	listint_t *newnode = malloc(sizeof(listint_t)), *turtle, *rabbit;
+
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = number;
+	newnode->next = NULL;
 
 	if (head == NULL)
-		return (NULL);
-
-	new = createNode(number);
-	if (new == NULL)
-		return (NULL);
-
-	if (*head == NULL)
-		*head = new;
-	else
 	{
-		new->next = *head;
-		*head = new;
+		head = &newnode;
+		return (newnode);
 	}
-
-	return (new);
+	if (*head == NULL)
+	{
+		(*head) = newnode;
+		return (newnode);
+	}
+	if ((*head)->n >= number)
+	{
+		newnode->next = (*head);
+		(*head) = newnode;
+		return (newnode);
+	}
+	turtle = (*head);
+	rabbit = (*head);
+	while (rabbit->next != NULL && rabbit->next->next != NULL)
+	{
+		rabbit = rabbit->next->next;
+		if (rabbit->n < number)
+			turtle = rabbit;
+		else if (turtle->next->n <= number)
+		{
+			newnode->next = turtle->next->next;
+			turtle->next->next = newnode;
+			return (newnode);
+		}
+		else if (turtle->n <= number)
+		{
+			newnode->next = turtle->next;
+			turtle->next = newnode;
+			return (newnode);
+		}
+	}
+	if (rabbit->next == NULL)
+	{
+		rabbit->next = newnode;
+		return (newnode);
+	}
+	if (rabbit->next->n <= number)
+	{
+		rabbit->next->next = newnode;
+		return (newnode);
+	}
+	newnode->next = rabbit->next;
+	rabbit->next = newnode;
+	return (newnode);
 }
